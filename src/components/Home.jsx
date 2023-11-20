29d44a3b10ab1260ddaaedc31676d60454ef8119
Jorge Gonzalez <jorge.1014@gmail.com>
2023-11-19 20:49:54 -0800

    updated homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,16 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
         
+        {/* COntainer */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center'>
+          <p>Hi Myname is</p>
+          <h1>Jorge Gonzalez</h1>
+          <h2>I'm a Full Stack Developer.</h2>
+          <p>i am a full stack developer focusing on my practicing my new coding skills.</p>
+
+        </div>
     </div>
   )
 }
