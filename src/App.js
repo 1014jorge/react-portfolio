fa90eaeea1c84081c4dcf379cbb601c638a7db59
Jorge Gonzalez <jorge.1014@gmail.com>
2023-11-19 21:53:49 -0800

    updated about and home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import { HiArrowNarrowRight } from 'react-icons/hi'
 
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
-        
-        {/* COntainer */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center'>
-          <p>Hi Myname is</p>
-          <h1>Jorge Gonzalez</h1>
-          <h2>I'm a Full Stack Developer.</h2>
-          <p>i am a full stack developer focusing on my practicing my new coding skills.</p>
 
+      {/* COntainer */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Hi My name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold-[#ccd6f6]'>Jorge Gonzalez</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>i am a full stack developer focusing on my practicing my new coding skills.</p>
+
+
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Projects
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
         </div>
+      </div>
     </div>
   )
 }
