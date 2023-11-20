55ed5c40ff348a9b089391f15a7115e44cc61b31
Jorge Gonzalez <jorge.1014@gmail.com>
2023-11-19 22:36:19 -0800

    updated app.js and Skillsjsx

---
+++ src/components/Skills.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Github from '../assets/github.png';
+import Javascript from '../assets/javascript.png';
+import Node from '../assets/node.png';
+import ReactImg from '../assets/react.png';
+import Tailwind from '../assets/tailwind.png';
+
+const Skills = () => {
+    return (
+        <div name='skills' className='bg-[#0a192f]'>
+            <div>
+                <div>
+                    <p>Experience</p>
+                    <p>These are the technoligies ive learned.</p>
+                </div>
+                <div>
+                    <div>
+                        <img className='w-20 mx auto' src='{HTML}' alt='HTML icon' />
+                        <p>HTML</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
