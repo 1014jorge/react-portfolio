ade760ab5e819fd0d4240245093f01f104b1f2bb
Jorge Gonzalez <jorge.1014@gmail.com>
2023-11-22 15:17:01 -0800

    updated skills jsk

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,14 +8,15 @@ import Tailwind from '../assets/tailwind.png';
 
 const Skills = () => {
     return (
-        <div name='skills' className='bg-[#0a192f]'>
-            <div>
+        <div name='skills' className='bg-[#0a192f] text-gray-300 py-4'>
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
-                    <p>Experience</p>
-                    <p>These are the technoligies ive learned.</p>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Experience</p>
+                    <p className='py-4'>These are the technoligies ive learned.</p>
                 </div>
-                <div>
-                    <div>
+
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                         <img className='w-20 mx auto' src='{HTML}' alt='HTML icon' />
                         <p>HTML</p>
                     </div>
@@ -25,4 +26,4 @@ const Skills = () => {
     )
 }
 
-export default Skills
+export default Skills;
