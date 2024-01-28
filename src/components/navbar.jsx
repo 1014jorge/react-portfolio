163292a6e3785fa1e8c0deb54ddf724e663db4b7
Jorge Gonzalez <jorge.1014@gmail.com>
2024-01-27 17:16:56 -0800

    Added resume and imported react pdf  updated nav bar comp

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
                         <a className="flex justify-between items-center w-full text-gray-300"
-                            href="/">
+                            href={process.env.PUBLIC_URL + "/fullstack2r.pdf"} target="_blank" rel="noopener noreferrer">
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
                     </li>
